55c72f39768f841215a33d96b965e3aac7e40171
motoloj <jeounghy1229@naver.com>
2022-03-09 20:44:58 +0900

    2022_03_09 study
    2022_03_09 study

---
+++ algorithm/greedy/1026.py
@@ -0,0 +1,13 @@
+# 2022_03_09_2
+# 보물
+n = int(input())
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+a = sorted(a)
+b = sorted(b, reverse=True)
+result = 0
+for i in range(n):
+    result += a[i]*b[i]
+print(result)
+
+# b 리스트 재배열  X 조건 => b의 최대값을 뽑아 곱해줌
--- algorithm/greedy/1931.py
+++ algorithm/greedy/1931.py
@@ -11,14 +11,13 @@ for i in range(n):
     time_list.append(list(map(int, sys.stdin.readline().split())))
 time_list.sort(key = lambda x : (x[1], x[0])) # 끝나는 시간 우선 정렬후 시작 시간순으로 정렬(작은순, 오름차순)
 
-count = 1
+count = 1 # 처음 시간은 무조건 회의실에 배정(시간정렬을 가장빨리 끝나고 가장 처음시작해서)포문이 2번째 즉, 인덱스상 1번부터 돌기 때문에 카운트를 미리 1처리
 initial_e = time_list[0][1] #가장 빨리 끝나는 시간에서 빠른 시작시간
-for i in range(n-1):
-    start, end = map(int, input().split())
-    if initial_e <= start:
-        result += 1
-        initial_e = end
-print(result)
+for i in range(1, n):
+    if initial_e <= time_list[i][0]:
+        initial_e = time_list[i][1]    # 다음 배정할 회의시간의 끝나는 시간을 엔드 타임 초기값으로 변경
+        count += 1
+print(count)
 
 # 변수.sort() ==> 리스트만 정렬,단순 문자열등은 정렬x 변수원본에 영향, sorted(변수) ==> 모든것 정렬 가능, 원본에 영향을 안줌
 # >>> tuple_list = [('좋은하루', 0),
