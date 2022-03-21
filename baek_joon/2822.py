3634df3fe58cf09352813f5aa5230f4801ff8e08
motoloj <jeounghy1229@naver.com>
2022-03-21 23:32:55 +0900

    2022_03_21 study
    2022_03_21 study

--- baek_joon/2822.py
+++ baek_joon/2822.py
@@ -1,6 +1,6 @@
 # 2022_03_21_1
 # 점수 계산
-# 1. max 값 pop후 값으로 인덱스 위치, 2. 정렬후 위치 순서대로 index출력
+# 1. max 값 pop후 값으로 인덱스 위치, OR 2. 정렬후 위치 순서대로 index출력
 num = [int(input()) for _ in range(8) ]
 sort_num = sorted(num, reverse=True)
 print(sum(sort_num[:5]))
