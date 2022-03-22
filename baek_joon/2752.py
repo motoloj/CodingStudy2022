9b3190fd7453c1de0b8387ba1a6eb23fac489d3b
motoloj <jeounghy1229@naver.com>
2022-03-22 21:28:37 +0900

    2022_03_22 study
    2022_03_22 study

---
+++ baek_joon/2752.py
@@ -0,0 +1,12 @@
+# 2022_03_22
+# 세수정렬
+print(*sorted(map(int, input().split())))
+
+# tmp = 0
+# num = list(map(int, input().split()))
+# for i in range(2):
+#     if num[i] > num[i+1]:
+#         tmp = num[i]
+#         num[i] = num[i+1]
+#         num[i+1] = tmp
+# print(*num)
