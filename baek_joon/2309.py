a6b520fa015df0cba0abd824f8d52ffbc0cc5459
motoloj <jeounghy1229@naver.com>
2022-03-27 15:37:41 +0900

    2022_03_27 study
    2022_03_27 study

---
+++ baek_joon/2309.py
@@ -0,0 +1,10 @@
+# 2022_03_27_1
+# 일곱 난쟁이
+# 9C7 = 9C2 = 9*8/2*1 = 36가지, 랜덤?
+import random
+num = [int(input()) for _ in range(9)]
+total = [0]
+while sum(total) != 100:
+    total = random.sample(num, 7)
+print(*sorted(total),sep='\n')
+# 전체 값을 더한 상태에서  포문으로 2개값만 빼서 찾는 경우도 있음.
