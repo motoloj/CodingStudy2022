80ffa557da2395a54bd44a43df14d8ffb885006e
motoloj <jeounghy1229@naver.com>
2022-04-03 14:15:53 +0900

    2022_04_03 study
    2022_04_03 study

--- baek_joon/3058.py
+++ baek_joon/3058.py
@@ -1,6 +1,18 @@
 # 2022_04_03_1
 # 짝수를 찾아라
+# 시간 164ms 
+
+# for _ in range(int(input())):
+#    even =  [i for i in map(int, input().split()) if i % 2 == 0 ]
+#    print(sum(even), min(even))
+   
+# ------------------------------ 시간 테스트
+# 메모리 30860kb로 동일, 시간 160ms로 더 적음
 
 for _ in range(int(input())):
-   even =  [i for i in map(int, input().split()) if i % 2 == 0 ]
-   print(sum(even), min(even))
+    even = []
+    num = map(int, input().split())
+    for i in num:
+        if i % 2 == 0:
+            even.append(i)
+    print(sum(even), min(even))
